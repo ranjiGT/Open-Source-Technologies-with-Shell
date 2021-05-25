b9b32c5d74c6b37ef2bc6c36886090bb155ba944
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-05-25 20:23:40 +0200

    Create arrayshell.sh

---
+++ arrayshell.sh
@@ -0,0 +1,14 @@
+# Shell Program to Print an Array
+clear
+echo "Enter number of elements: "
+read n
+echo "Enter array elements: "
+for ((i=0; i<n; i++))
+do
+read a[$i]
+done
+echo "The elements of an array are: "
+for ((i=0; i<n; i++))
+do
+echo ${a[$i]}
+done
