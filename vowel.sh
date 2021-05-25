0a31f394e53c225f525ab15d3e8bbade49da04ec
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-05-25 20:21:47 +0200

    Create vowel.sh

---
+++ vowel.sh
@@ -0,0 +1,12 @@
+# Shell Program to Find whether the Character is Vowel or Not
+clear
+echo "Enter any character: "
+read ch
+case $ch in
+"a") echo "It is a vowel.";;
+"e") echo "It is a vowel.";;
+"i") echo "It is a vowel.";;
+"o") echo "It is a vowel.";;
+"u") echo "It is a vowel.";;
+*) echo "It is not a vowel."
+esac
