f309d78df5ba43435ef4db23810ed2d30350d322
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-05-25 20:24:08 +0200

    Create factor.sh

---
+++ factor.sh
@@ -0,0 +1,11 @@
+# Shell Program to Print Factorial of a Number
+clear
+i=1
+fact=1
+echo "Enter any number: "
+read n
+for (( i=1; i<=n; i++ ))
+do
+fact=`expr $fact \* $i`
+done
+echo "Factorial = $fact"
