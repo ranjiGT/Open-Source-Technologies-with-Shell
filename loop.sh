21bd96a8c9aaab8804b1699c623c36769d30d1a6
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-05-25 20:22:09 +0200

    Create loop.sh

---
+++ loop.sh
@@ -0,0 +1,10 @@
+# Shell Program to Implement If Statement
+clear
+echo "Enter any number: "
+read n
+if [ $n -le 10 ]
+then
+echo "Number is less than or equal to 10."
+else
+echo "Number is greater than 10."
+Output:
